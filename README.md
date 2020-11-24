372a9d5a0478270248febf59feafa7713f214219
Luke Franzke <lukefranzke@gmail.com>
2020-11-24 12:22:24 +0100

    Minor Updates

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ The processing template for 2020 Graphic Design Basics module.
 
 ![Posters](/Raw/JT_Poster.gif?raw=true)  
 ![Posters](/Raw/RC_DS_Gif_Animation.gif?raw=true)  
+
+Screen setup:
+For running on multiple displays, displayplacer is required for correct positioning after restart. https://github.com/jakehilborn/displayplacer
---
+++ screen_setup/runSetup.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+displayplacer "id:C6143168-CFCA-36D8-C34F-8FE0FD9F6E5A res:1080x1920 hz:60 color_depth:8 scaling:off origin:(1080,0) degree:90" "id:3E8D6A68-B132-6AA1-BB77-EF632DDFB150 res:1080x1920 hz:60 color_depth:8 scaling:off origin:(0,0) degree:90" "id:F1692720-C59D-26E7-F7A7-115012FCB5A5 res:1280x800 hz:60 color_depth:8 scaling:off origin:(2160,908) degree:0"
